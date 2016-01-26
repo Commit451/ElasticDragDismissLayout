c5877bde5c3a850bdbd8fbd6e07d31a797872945
Jawnnypoo <jawnnypoo@gmail.com>
2016-01-26 15:34:10 -0600

    Create an example for RecyclerView also

---
+++ app/src/main/java/com/commit451/elasticdragdismisslayout/sample/DetailRecyclerViewActivity.java
@@ -0,0 +1,111 @@
+package com.commit451.elasticdragdismisslayout.sample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.commit451.elasticdragdismisslayout.ElasticDragDismissFrameLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Testing using a RecyclerView
+ */
+public class DetailRecyclerViewActivity extends AppCompatActivity {
+
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, DetailRecyclerViewActivity.class);
+        return intent;
+    }
+
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.draggable_frame) ElasticDragDismissFrameLayout mDragFrameLayout;
+    @Bind(R.id.recyclerview) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recyclerview);
+        ButterKnife.bind(this);
+        mToolbar.setTitle("Countries");
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_24dp);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        CountriesAdapter adapter = new CountriesAdapter(this);
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mDragFrameLayout.addListener(
+            new SystemChromeFader(getWindow()) {
+                @Override
+                public void onDragDismissed() {
+                    // if we drag dismiss downward then the default reversal of the enter
+                    // transition would slide content upward which looks weird. So reverse it.
+                    if (mDragFrameLayout.getTranslationY() > 0) {
+                        //TODO
+    //                            getWindow().setReturnTransition(
+    //                                    TransitionInflater.from(AboutActivity.this)
+    //                                            .inflateTransition(R.transition.about_return_downward));
+                    }
+                    finishAfterTransition();
+                }
+            });
+    }
+
+    static class CountriesAdapter extends RecyclerView.Adapter<CountriesViewHolder> {
+
+        String[] mCountries;
+        public CountriesAdapter(Context context) {
+            mCountries = context.getResources().getStringArray(R.array.countries);
+
+        }
+
+        @Override
+        public CountriesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return CountriesViewHolder.inflate(parent);
+        }
+
+        @Override
+        public void onBindViewHolder(CountriesViewHolder holder, int position) {
+            holder.bind(mCountries[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCountries.length;
+        }
+    }
+
+    static class CountriesViewHolder extends RecyclerView.ViewHolder {
+
+        public static CountriesViewHolder inflate(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.viewholder_item, parent, false);
+            return new CountriesViewHolder(view);
+        }
+
+        @Bind(R.id.text) TextView mText;
+
+        public CountriesViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        public void bind(String country) {
+            mText.setText(country);
+        }
+    }
+}
--- app/src/main/java/com/commit451/elasticdragdismisslayout/sample/MainActivity.java
+++ app/src/main/java/com/commit451/elasticdragdismisslayout/sample/MainActivity.java
@@ -3,15 +3,25 @@ package com.commit451.elasticdragdismisslayout.sample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @OnClick(R.id.clickable)
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+
+    @OnClick(R.id.nestedscrollview)
     void onClickText() {
         Intent intent = DetailActivity.newIntent(MainActivity.this);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.recyclerview)
+    void onRecyclerViewExampleClick() {
+        Intent intent = DetailRecyclerViewActivity.newIntent(this);
         startActivity(intent);
     }
 
@@ -20,5 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mToolbar.setTitle("ElasticDragDismissLayout");
     }
 }
